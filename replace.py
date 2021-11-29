50e27400cf3bd9af2c9784699196c2a49b1d9f0f
Maximilian Schmeller <m.schmeller@tum.de>
2021-11-29 18:48:16 +0100

    first commit

---
+++ replace.py
@@ -0,0 +1,22 @@
+# encoding: utf-8
+
+with open("input.txt", "r", encoding="utf-8") as f:
+    text = f.read()
+
+kanji = set()
+
+for char in text:
+    if 0x9faf >= ord(char) >= 0x4e00:
+        kanji.add(char)
+
+
+replacements = {}
+
+for (i, kan) in enumerate(kanji):
+    replacements[kan] = chr(0x1F400 + i)
+
+for (k, v) in replacements.items():
+    text = text.replace(k, v)
+
+with open("output.txt", "w", encoding="utf-8") as f:
+    f.write(text)
